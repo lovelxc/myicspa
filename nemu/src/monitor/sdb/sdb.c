f11b7dd278df868518cec8fbaef286e68fb7a87c
tracer-ics2021 <tracer@njuics.org>
2022-06-10 15:02:35 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: 2fb5bcccaa1945f3a4889f1f265753e8     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-113-generic       Architecture: x86-64  15:02:35 up  1:41,  0 users,  load average: 0.06, 0.07, 0.02

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -1,6 +1,6 @@
 #include <isa.h>
 #include <cpu/cpu.h>
-#include <cpu/decode.h>
+#include <cpu/exec.h>
 #include <utils.h>
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -40,22 +40,23 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_si(char *args) {
-  // switch (nemu_state.state) {
-  //   case NEMU_END: case NEMU_ABORT:
-  //     printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
-  //     return -1;
-  //   default: nemu_state.state = NEMU_RUNNING;
-  // }
-  // int n = 0;
-  // if(strlen(args) > 0) sscanf(args, "%d", &n);
-  // Log("%d", n);
+  switch (nemu_state.state) {
+    case NEMU_END: case NEMU_ABORT:
+      printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
+      return -1;
+    default: nemu_state.state = NEMU_RUNNING;
+  }
+  int n = 0;
+  if(strlen(args) > 0) sscanf(args, "%d", &n);
+  Log("%d", n);
   // Decode s;
-  // while(n--){
-  //   fetch_decode_exec_updatepc(&s);
-  //   trace_and_difftest(&s, cpu.pc);
-  //   if (nemu_state.state != NEMU_RUNNING) break;
-  //   // IFDEF(CONFIG_DEVICE, device_update());
-  // }
+  while(n--){
+    // fetch_decode_exec_updatepc(&s);
+    // trace_and_difftest(&s, cpu.pc);
+    cpu.pc = 1;
+    if (nemu_state.state != NEMU_RUNNING) break;
+    // IFDEF(CONFIG_DEVICE, device_update());
+  }
   
   return 0;
 }
