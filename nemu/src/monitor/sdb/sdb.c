00a39e842581fedebd5d983c88030b90b755979b
tracer-ics2021 <tracer@njuics.org>
2022-06-10 15:20:43 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: 2fb5bcccaa1945f3a4889f1f265753e8     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-113-generic       Architecture: x86-64  15:20:43 up  1:59,  0 users,  load average: 0.35, 0.20, 0.12

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -1,6 +1,6 @@
 #include <isa.h>
 #include <cpu/cpu.h>
-// #include <cpu/exec.h>
+#include <cpu/exec.h>
 #include <utils.h>
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -49,13 +49,6 @@ static int cmd_si(char *args) {
   int n = 0;
   if(strlen(args) > 0) sscanf(args, "%d", &n);
   Log("%d", n);
-  // Decode s;
-  // while(n--){
-  //   fetch_decode_exec_updatepc(&s);
-  //   trace_and_difftest(&s, cpu.pc);
-  //   if (nemu_state.state != NEMU_RUNNING) break;
-  //   // IFDEF(CONFIG_DEVICE, device_update());
-  // }
   
   return 0;
 }
