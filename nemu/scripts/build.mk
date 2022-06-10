a17be8fdfa2ea92799f4f346a16844728ed4d206
tracer-ics2021 <tracer@njuics.org>
2022-06-10 17:19:37 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: 2fb5bcccaa1945f3a4889f1f265753e8     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-113-generic       Architecture: x86-64  17:19:37 up  3:58,  0 users,  load average: 0.09, 0.04, 0.01

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -34,7 +34,7 @@ $(OBJ_DIR)/%.o: %.c
 	@$(CC) $(CFLAGS) -c -o $@ $<
 	$(call call_fixdep, $(@:.o=.d), $@)
 
-$(OBJ_DIR)/sdb.o:$(NEMU_HOME)/src/monitor/sdb.c $(NEMU_HOME)/src/cpu/cpu-exec.c
+$(OBJ_DIR)/sdb.o:$(OBJ_DIR)/sdb.o $(OBJ_DIR)/cpu-exec.o
 	@echo + CC1 sdb.o <- sdb.c cpu-exec.c
 	@$(CC) $(CFLAGS) -c -o sdb.c cpu-exec.c
 
