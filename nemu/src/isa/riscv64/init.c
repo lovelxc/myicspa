a6ceb2e8c73cc8765b9ebd0985e3043d9f4fb385
tracer-ics2021 <tracer@njuics.org>
2022-06-11 20:21:35 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: c7e72e9c65824603942d6a04ffb151fd     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-113-generic       Architecture: x86-64  20:21:35 up 24 min,  1 user,  load average: 0.00, 0.01, 0.08

--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -5,7 +5,7 @@
 // but it is ok since we do not access the array directly
 static const uint32_t img [] = {
   0x00000297,  // auipc t0,0
-  0x0002b823,  // sd  zero,16(t0)
+  0x802b823,  // sd  zero,16(t0)
   0x0102b503,  // ld  a0,16(t0)
   0x0000006b,  // nemu_trap
   0xdeadbeef,  // some data
