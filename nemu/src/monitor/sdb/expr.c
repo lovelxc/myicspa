63f38a36cc96e577b090b9e7ea2d857ac7ed7d90
tracer-ics2021 <tracer@njuics.org>
2022-06-11 23:58:56 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: c7e72e9c65824603942d6a04ffb151fd     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-113-generic       Architecture: x86-64  23:58:56 up  4:01,  1 user,  load average: 0.22, 0.27, 0.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -21,6 +21,7 @@ static struct rule {
    * Pay attention to the precedence level of different rules.
    * 添加寄存器的识别模式，目前十进制常数不能为0
    */
+  {"\\s+", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"\\-", '-'},         // minus
   {"\\*", '*'},         // muti
@@ -28,7 +29,7 @@ static struct rule {
   {"\\(", '('},         // left parentheses
   {"\\)", ')'},	        // right parentheses
   {"==", TK_EQ},        // equal
-  {"\\s+", TK_NOTYPE},    // spaces
+  
   {"([1-9][0-9]*)", TK_NUM_10},				  // 8: 10num
   {"0[xX]([0-9a-fA-F]{1,8})", TK_NUM_16},				  // 9: 16num
 };
