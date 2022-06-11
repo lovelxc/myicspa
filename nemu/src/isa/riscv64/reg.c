6de29b871ecdfb24380132d8c2a6c716d3a72390
tracer-ics2021 <tracer@njuics.org>
2022-06-11 20:11:28 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: c7e72e9c65824603942d6a04ffb151fd     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-113-generic       Architecture: x86-64  20:11:28 up 14 min,  1 user,  load average: 0.03, 0.13, 0.17

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -9,10 +9,11 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-  // bool t;
+  bool t;
   for(int i = 0; i < 32; ++i){
-    printf("%-6s %ld\n", reg_name(i,4), gpr(i));
+    printf("%-6s %ld\n", reg_name(i,4), isa_reg_str2val(reg_name(i,4),t));
   }
+  printf("PC:%-6s %ld\n", (uint64_t)cpu.pc);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
