92c7b3bbf754c9ad34e9c5426d37e657567953f7
tracer-ics2021 <tracer@njuics.org>
2022-06-11 20:52:56 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: c7e72e9c65824603942d6a04ffb151fd     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-113-generic       Architecture: x86-64  20:52:56 up 55 min,  1 user,  load average: 0.13, 0.11, 0.07

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -6,7 +6,7 @@ void engine_start();
 int is_exit_status_bad();
 
 int main(int argc, char *argv[]) {
-  FILE * fp=freopen("in.txt","r",stdin);
+  // FILE * fp=freopen("in.txt","r",stdin);
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
 
   /* Start engine. */
   engine_start();
-  fclose(fp);
+  // fclose(fp);
   if(is_exit_status_bad()){
 	  Log("exit_status is bad!");
   }
