e5695cbe9317ad3427d8c9b0ee55057635b3b734
tracer-ics2021 <tracer@njuics.org>
2022-06-12 16:22:48 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: f9bc2e940195486d9bfae2b289d68e9d     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-117-generic       Architecture: x86-64  16:22:48 up  2:39,  2 users,  load average: 0.01, 0.04, 0.07

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -178,7 +178,9 @@ static word_t eval(int p, int q){
       case '+': return val1 + val2;
       case '-': return val1 - val2;
       case '*': return val1 * val2;
-      case '/': return val1 / val2;
+      case '/': 
+        if(val2==0) return -1;
+        else return val1 / val2;
       default: panic("error type found");
     }
   }
