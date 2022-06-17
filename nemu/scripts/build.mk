b9c0d2bc7bfbb70e008f034268629b6d84212cee
tracer-ics2021 <tracer@njuics.org>
2022-06-17 11:19:17 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: ab1acb64723b43bb8d5cb519d0b169fd     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-117-generic       Architecture: x86-64  11:19:17 up 32 min,  2 users,  load average: 0.16, 0.08, 0.09

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -37,7 +37,7 @@ $(OBJ_DIR)/%.o: %.c
 $(OBJ_DIR)/%.o: %.cc
 	@echo + CXX $<
 	@mkdir -p $(dir $@)
-	@$(CXX) $(CFLAGS) $(CXXFLAGS) -c -o -E $@ $<
+	@$(CXX) $(CFLAGS) $(CXXFLAGS) -c -o $@ $<
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 # Depencies
