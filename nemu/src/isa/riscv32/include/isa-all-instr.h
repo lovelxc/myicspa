218ee349f188284767e0969472df915a7e8ce606
lovelxc <446064194@qq.com>
2022-06-23 12:32:54 +0800

    卡在jump指令，还不了解怎么形成signed imm

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -2,6 +2,6 @@
 #include "../local-include/rtl.h"
 
 #define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(nemu_trap)\
-f(auipc)
+f(auipc) f(jal) f(jalr) 
 
 def_all_EXEC_ID(); // -> enum { MAP(INSTR_LIST, def_EXEC_ID) TOTAL_INSTR }
