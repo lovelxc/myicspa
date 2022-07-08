96f52758471a1abe5b6a7db792d862e3896996de
tracer-ics2021 <tracer@njuics.org>
2022-07-08 19:16:02 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: 6034026caec44826ae0fc32764c24327     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-122-generic       Architecture: x86-64  19:16:02 up  3:49,  1 user,  load average: 0.22, 0.06, 0.02

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -13,6 +13,6 @@ def_EHelper(addi) {
   // ddest <- pc + imm
   *s0 = id_src2->simm;
   rtl_sext(s, s1, s0, 12);
-  printf("%d %d\n", *dsrc1, *s1);
+  printf("src1:%d imm:%d\n", *dsrc1, *s1);
   rtl_addi(s, ddest, dsrc1, *s1);
 }
