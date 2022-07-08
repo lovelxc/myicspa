227bc35066f01ee07a8416620889595b1a2e951d
tracer-ics2021 <tracer@njuics.org>
2022-07-08 17:19:32 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: 6034026caec44826ae0fc32764c24327     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-122-generic       Architecture: x86-64  17:19:32 up  1:52,  1 user,  load average: 0.18, 0.12, 0.07

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -29,7 +29,7 @@ static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t* src1) {
 static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   if((1 << (width - 1)) & (*src1)){
-    printf("2");
+    printf("%x", (*src1));
     *dest = *src1 << (sizeof(rtlreg_t) - width);
     *dest = *dest >> (sizeof(rtlreg_t) - width);
   }
