c2a939ccdfd8612758e801b4c0a621bee9a13f8a
tracer-ics2021 <tracer@njuics.org>
2022-07-08 20:41:50 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: 6034026caec44826ae0fc32764c24327     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-122-generic       Architecture: x86-64  20:41:50 up  5:15,  1 user,  load average: 0.17, 0.07, 0.01

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -3,15 +3,15 @@
 def_EHelper(jal) {
   // ddest <- pc + 4
   *ddest = s->snpc;
-  printf("jump to:%x\n", (word_t)(id_src1->simm + s->pc));
   rtl_j(s, id_src1->simm + s->pc);
 }
 
 // I-type
 def_EHelper(jalr) {
   *ddest = s->snpc;
-  *s0 = *dsrc2;
-  rtl_sext(s, s1, s0, 12); // sext(offset))
+  // *s0 = *dsrc2;
+  // rtl_sext(s, s1, s0, 12); // sext(offset))
+  printf("%d %d", *dsrc1 , id_src2->imm);
   *s1 = (*dsrc1 + id_src2->imm) & (~(sword_t)1);
   printf("%x\n", (word_t)*s1);
   rtl_jr(s, s1);
