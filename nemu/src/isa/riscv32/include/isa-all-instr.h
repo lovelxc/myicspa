a1b4f33af1a64df2d4ff00014de934820628a14c
tracer-ics2021 <tracer@njuics.org>
2022-07-28 16:52:23 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: 04dac3075a4a4bcdab6c9b2a07a19977     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-122-generic       Architecture: x86-64  16:52:23 up 44 min,  1 user,  load average: 0.00, 0.00, 0.01

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -2,7 +2,8 @@
 #include "../local-include/rtl.h"
 
 #define INSTR_LIST(f) f(lui) f(lw) f(sw) f(inv) f(nemu_trap)\
-f(auipc) f(jal) f(jalr) f(lhu) f(addi) f(beq) f(add) f(sub)\
-f(slti) f(sltiu)
+f(auipc) f(jal) f(jalr) f(lhu) f(addi)  f(add) f(sub)\
+f(slti) f(sltiu)\
+f(beq) f(bne) f(blt) f(bge) f(bltu) f(bgeu)
 
 def_all_EXEC_ID(); // -> enum { MAP(INSTR_LIST, def_EXEC_ID) TOTAL_INSTR }
