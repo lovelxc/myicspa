fc336d270a4a36c7eb5792e6634de7446ce42790
tracer-ics2021 <tracer@njuics.org>
2022-08-05 02:48:50 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: e86f788e73024b2da0335fed70cc1804     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-122-generic       Architecture: x86-64  02:48:50 up 50 min,  1 user,  load average: 0.02, 0.03, 0.02

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,7 +5,10 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  if(s == NULL) return 0;
+  size_t i = 0;
+  while(*s != '\0') ++s, ++i;
+  return i;
 }
 
 char *strcpy(char *dst, const char *src) {
@@ -17,11 +20,22 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *d = dst;
+  while(*d != '\0') ++d;
+  for(const char *s = src; *s != '\0'; s++, d++){
+    *d = *src;
+  }
+  *d = '\0';
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  for(const char *tmp1 = s1, *tmp2 = s2; !(*tmp1 == '\0' && *tmp2 == '\0'); ++tmp1, ++tmp2){
+    if(*tmp1 == '\0') return -1;
+    else if (*tmp2 == '\0') return 1;
+    if(*tmp1 != *tmp2) return (*tmp1 < *tmp2) ? -1 : 1;
+  }
+  return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +43,12 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  if(s == NULL || n <= 0) return 0;
+  unsigned char *tmp = s, t = c;
+  while(n--){
+    *tmp = t, ++tmp;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +60,12 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  if(n <= 0) return 0;
+  size_t i = 0;
+  for(const unsigned char *tmp1 = s1, *tmp2 = s2; i < n; ++tmp1, ++tmp2, ++i){
+    if(*tmp1 != *tmp2) return (*tmp1 < *tmp2) ? -1 : 1;
+  }
+  return 0;
 }
 
 #endif
