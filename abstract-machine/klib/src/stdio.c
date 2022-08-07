e4ac621dc82c2a7b9319b650ed62fe324e90cc40
tracer-ics2021 <tracer@njuics.org>
2022-08-08 00:15:22 +0800

    >  compile 2018172019    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: f7e96656c0c541f6a69a6e2590454d10            Boot ID: 167559d847564859843ba36296e656d5     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.4.0-122-generic       Architecture: x86-64  00:15:22 up  4:13,  0 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -11,55 +11,63 @@ int printf(const char *fmt, ...) {
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
+  int d;
+  char c;
+  char *s;
+  char buf[34];
+  
+  while (*fmt){
+    if(*fmt == '%'){
+      fmt++;
+      switch(*fmt){
+        case 's': /* string */
+          s = va_arg(ap, char *);
+          while(*s){
+            *out++ = *s++;
+          }
+          break;
+        case 'd': /* int */
+          d = va_arg(ap, int);
+          memset(buf, 0, sizeof(buf));
+          if(d < 0){
+            *out++ = '-';
+            d = -d;
+          }
+          if(d == 0) *out++ = '0';
+          else{
+            int len = 0;
+            while(d != 0){
+              buf[len++] = d%10 + '0';
+              d /= 10;
+            }
+            while(len > 0){
+              *out++ = buf[len--];
+            }
+          }
+          break;
+        case 'c': /* char */
+          /* need a cast here since va_arg only
+            takes fully promoted types */
+          c = (char)va_arg(ap, int);
+          *out++ = c;
+          break;
+        default:
+          *out++ = *fmt;
+      }
+    }
+    else{
+      *out++ = *fmt;
+    }
+    ++fmt;
+  }
 }
 
 int sprintf(char *out, const char *fmt, ...) {
   if(fmt == NULL) return -1;
   char *res = out;
   va_list ap;
-  int d;
-  char c;
-  char *s;
-  char buf[34];
   va_start(ap, fmt);
-  while (*fmt){
-    switch (*fmt){
-      case 's': /* string */
-        s = va_arg(ap, char *);
-        while(*s){
-          *out++ = *s++;
-        }
-        break;
-      case 'd': /* int */
-        d = va_arg(ap, int);
-        memset(buf, 0, sizeof(buf));
-        if(d < 0){
-          *out++ = '-';
-          d = -d;
-        }
-        if(d == 0) *out++ = '0';
-        else{
-          int len = 0;
-          while(d != 0){
-            buf[len++] = d%10 + '0';
-            d /= 10;
-          }
-          while(len > 0){
-            *out++ = buf[len--];
-          }
-        }
-        break;
-      case 'c': /* char */
-        /* need a cast here since va_arg only
-          takes fully promoted types */
-        c = (char)va_arg(ap, int);
-        *out++ = c;
-        break;
-      default:
-        *out++ = *fmt;
-    }
-    ++fmt;
-  }
+  vsprintf(out, fmt, ap);
   va_end(ap);
   return strlen(res);
 }
